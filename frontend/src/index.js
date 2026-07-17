5cb2937f6787f319bb406ee4c10e792041c4135d
Karanshika <karanraj83671@gmail.com>
2026-07-17 13:03:33 +0530

    Fix API baseURL for production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import './index.css';
 
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || '';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
